90db9c6cc203ece891b26daabd1bc61df662f278
Sumire Doi <m245461@hiroshima-u.ac.jp>
2025-02-27 15:25:26 +0900

    fix: 余計な空白を削除

--- service/schema/user_schema.py
+++ service/schema/user_schema.py
@@ -5,4 +5,3 @@ class UserUpdateSchema(BaseModel):
     """ユーザー更新のリクエストスキーマ"""
     name: Optional[str] = Field(None, min_length=1, max_length=200, description="ユーザー名")
     email: Optional[EmailStr] = Field(None, description="メールアドレス")
-    
