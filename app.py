f0e089f0b9f41f15f872ee61891fb2cd5206f781
shima004 <yellow030408@gmail.com>
2025-02-28 01:21:17 +0900

    feat: アプリケーションのエントリーポイントを追加し、直接実行可能に変更

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ app.register_blueprint(tree_blueprint)
 app.register_blueprint(follow_blueprint)
 app.register_blueprint(user_tree_dream_blueprint)
 
-app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
