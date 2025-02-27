e36d54ca2a01615a06e0a082cc91f5258931e757
Sumire Doi <m245461@hiroshima-u.ac.jp>
2025-02-27 17:22:23 +0900

    feat: /follow/エンドポイントを全部追加

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from domain.tables import create_tables
 from handler.auth_handler import auth
 from handler.user_handler import user
 from handler.tree_handler import user_tree_blueprint, tree_blueprint
+from handler.follow_handler import follow
+
 
 if __name__ == '__main__':
     create_tables()
@@ -11,5 +13,6 @@ if __name__ == '__main__':
     app.register_blueprint(user)
     app.register_blueprint(user_tree_blueprint)
     app.register_blueprint(tree_blueprint)
+    app.register_blueprint(follow)
 
     app.run(debug=True, host="0.0.0.0", port=5000)
